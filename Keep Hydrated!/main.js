9ae48fa604300aefc18c1c2982a2b5323cc8f9d7
jp2435 <fuseta00@gmail.com>
2021-12-05 21:38:34 +0000

    Keep Hydrated! Kata

---
+++ Keep Hydrated!/main.js
@@ -0,0 +1,19 @@
+function litres(time) {
+    return Math.trunc(0.5*time)
+}
+
+/*
+const litres = (time) => {
+  return Math.trunc(0.5*time)
+}
+*/
+
+/*
+function litres(time) {
+    const litres = Math.trunc(0.5 * Number(time))
+    console.log(litres)
+    return 0;
+}
+
+litres(11.8)
+*/
